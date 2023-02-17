b522769054469d6286e9483657366416478aec78
Rejoy01 <rejoywilson65@gmail.com>
2023-02-17 13:23:23 +0530

    first commit

---
+++ JsArrayMethods/Array.js
@@ -0,0 +1,23 @@
+const fruits = ["Banana","Orange","Apple","Mango"]
+
+// Array Pop Method
+//remove last element from an array
+fruits.pop()
+
+// array adds a new element to an array
+fruits.push("Kiwi")
+
+//removes first element from an array
+fruits.shift()
+
+//adds a new element to an array at the beginning
+fruits.unshift("Lemon")
+
+// array index
+
+fruits[0] = "kiwi"
+//adds a element at 0 the index
+
+//The length property provides an easy way to append a new element to an array:
+ 
+fruits[fruits.length] = "strawberry"
