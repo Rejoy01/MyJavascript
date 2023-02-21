c30f728340d8f4bb63490277ca957bea56987ecb
Rejoy01 <rejoywilson65@gmail.com>
2023-02-22 00:11:12 +0530

    updated

---
+++ Js_If_and_switch/If_statement.js
@@ -0,0 +1,25 @@
+// sytnax 
+
+if(condition){
+    //block of code to be executed when condition is true
+}
+
+if(hour < 18){
+    greeting = "Good day"
+}
+
+// else Statement 
+
+if(condtion){
+    // block of code to be executed when condition is true
+}else{
+    // block of code to be executed when condition is false
+}
+
+if(condition1){
+    // block of code to be executed when condition is TRUE
+}else if(condition2){
+    // block of code to be executed when condition1 is false and condition2 is true
+}else{
+    // block of code to be executed when condition1 is false and condition2 is false
+}
---
+++ Js_If_and_switch/switch_statement.js
@@ -0,0 +1,17 @@
+// Switch Statement syntax
+
+switch(expression){
+    case x :
+        // code block
+        break;
+    case y:
+        // code block
+        break;
+    case z :
+        //code block
+        break;
+    default:
+    // code block
+}
+
+//When JavaScript reaches a break keyword, it breaks out of the switch block.
