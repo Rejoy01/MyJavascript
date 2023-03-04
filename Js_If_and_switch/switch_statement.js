b5f0bb579b7aba2de1a5df99105f8ae4dc36630a
Rejoy01 <rejoywilson65@gmail.com>
2023-03-04 16:07:13 +0530

    updated

--- Js_If_and_switch/switch_statement.js
+++ Js_If_and_switch/switch_statement.js
@@ -1,17 +1,18 @@
 // Switch Statement syntax
 
-switch(expression){
-    case x :
-        // code block
-        break;
-    case y:
-        // code block
-        break;
-    case z :
-        //code block
-        break;
-    default:
-    // code block
-}
+// switch(expression){
+//     case x :
+//         // code block
+//         break;
+//     case y:
+//         // code block
+//         break;
+//     case z :
+//         //code block
+//         break;
+//     default:
+//     // code block
+// }
 
 //When JavaScript reaches a break keyword, it breaks out of the switch block.
+ 
---
+++ Object_methods/prototypes.js
@@ -0,0 +1,23 @@
+function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+  }
+
+// we cannot add a property to an existing object constructor
+
+//prototype inheritence 
+
+//All Javascript objects inherit properties and methods from the prototype
+
+// The javascript prototype property allows you to add new properties to object constructors
+
+
+Person.prototype.nationality = "English"
+
+const NewPerson = new Person("Rejo","wilson",24,"blue")
+
+console.log(NewPerson.nationality);
+
+//English
