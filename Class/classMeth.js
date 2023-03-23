23ce302645207a9bb63e05eee297601556da3b7b
Rejoy01 <rejoywilson65@gmail.com>
2023-03-23 23:25:06 +0530

    class

---
+++ Class/classMeth.js
@@ -0,0 +1,17 @@
+// Class methods are created with the same syntax as object methods.
+
+// Use the keyword class to create a class.
+
+// Always add a constructor() method.
+
+// Then add any number of methods.
+
+//Syntax
+
+// class ClassName {
+//     constructor() { ... }
+//     method_1() { ... }
+//     method_2() { ... }
+//     method_3() { ... }
+//   }
+
