6b83922b76c8c16f7d41fdbecf8d81c33764dc1c
Rejoy01 <rejoywilson65@gmail.com>
2023-05-09 21:09:16 +0530

    array_sort

--- js_function/array_sort.js
+++ js_function/array_sort.js
@@ -3,3 +3,11 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.sort();
 fruits.reverse();
+
+//sorting numbers will cause an error we can fix it by adding compare function
+
+const points =[40,100,1,5,25,10]
+
+points.sort(function(a,b){return a-b});
+
+console.log(points);
