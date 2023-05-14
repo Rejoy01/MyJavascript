df547cd59194fc33fa38419d31324a84a1eb37f3
Rejoy01 <rejoywilson65@gmail.com>
2023-05-14 22:33:41 +0530

    Errors added

--- errors/errors.js
+++ errors/errors.js
@@ -11,3 +11,28 @@ try {
     console.log(error.name);
 }
 
+//syntax error
+try {
+    eval("alert('Hello)");// missing ' will produce syntax error
+} catch (error) {
+    console.log(error.name);
+}
+
+// ReferenceError
+
+// occur if u use  (reference) a variable that has not been declared
+let x = 5;
+try {
+    x = y + 1;
+} catch (error) {
+    console.log(error.name);
+}
+
+//Type error
+
+let num = 1;
+try {
+    num.toUpperCase(); // you cannot convert a number to upper case
+} catch (error) {
+    console.log(error.name);
+}
