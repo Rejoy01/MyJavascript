777fd34b5e9bee034be91cf46bb81559f7f958fc
Rejoy01 <rejoywilson65@gmail.com>
2023-05-15 23:48:43 +0530

    number

---
+++ NumberMethod/num.js
@@ -0,0 +1,14 @@
+//toExponential()
+
+let x = 9.656
+
+console.log(x.toExponential(2));
+
+//toFixed()
+// toFixed() returns a string, with the number written with a specified number of decimals:
+
+console.log(x.toFixed(2));
+
+//toPrecision()
+//toPrecision() returns a string, with a number written with a specified length:
+console.log(x.toPrecision(6));
