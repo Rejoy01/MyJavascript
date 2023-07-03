f9cb8b6493835543886ecdc39416b09359cfeb68
Rejoy01 <rejoywilson65@gmail.com>
2023-07-04 00:57:17 +0530

    syntax

---
+++ syntax/syntax.js
@@ -0,0 +1,24 @@
+// javascript Literals
+
+//Numbers are written with or without decimals :
+//10
+//10.11
+
+// steings are text , written within double or single quotes 
+
+"jhon doe"
+
+'jhon doe'
+
+// in programming languages, variables are used to store data values 
+
+//javascript uses the keywords var  ,let and const to declare variables
+
+let x ;
+
+x = 6
+
+console.log(x);
+
+
+
