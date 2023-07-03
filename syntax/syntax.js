74679168542e24528fac01475f43451bc33d69e8
Rejoy01 <rejoywilson65@gmail.com>
2023-07-04 02:11:03 +0530

    syntax

--- syntax/syntax.js
+++ syntax/syntax.js
@@ -20,5 +20,10 @@ x = 6
 
 console.log(x);
 
+// expressions Are combinations of values , variables and operators 
+
+// keywords are used to identify actions to be performed 
+
+// eg let or var or const keyword tells the browser to create a variable
 
 
