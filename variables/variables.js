036f18e993175fde1484722f3caab5a80cf2ebd0
Rejoy01 <rejoywilson65@gmail.com>
2023-07-07 12:04:19 +0530

    variables

--- variables/variables.js
+++ variables/variables.js
@@ -1,5 +1,7 @@
 // javascript variables are declared in 4 ways :
 
+// variables are containers for storing variables
+
 // automatically 
 // using var 
 // using let 
@@ -22,3 +24,20 @@ console.log(c);
 
 // always use const if the value should not be changed
 // always use const if the type should not be changed (Arrays and Objects)
+
+// mixed examples
+
+const price1 = 5
+const price2 = 6
+let total = price1+price2;
+
+console.log(total);
+
+// value of price1 and price2 can't be chnage while it is declared with const
+// here value total can be changed as it is declared with let
+
+// all javascript variables must be identified with unique names.
+
+// these unique names are called identifiers
+
+// creating a variable in javascript is called declaring a variable
