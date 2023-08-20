887a9b826553f40502b3c28773ca09478aebc047
Rejoy01 <rejoywilson65@gmail.com>
2023-08-20 23:33:28 +0530

    ds_array

--- Destructuring_Tricks/ds_Array.js
+++ Destructuring_Tricks/ds_Array.js
@@ -2,6 +2,12 @@ const scores = [100,200,300,400]
 
 const [x,y,z] = scores
 
+const Emp = ["Emp1","Emp2","Emp3"]
+
+const [emp1,emp2,epm3] = Emp;
+
+console.log(emp1);
+
 console.log(x);
 //100
 console.log(y);
