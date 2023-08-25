1b73f70c4d04cd66b449ea24ad3b32fb0c2657e8
Rejoy01 <rejoywilson65@gmail.com>
2023-08-25 16:48:49 +0530

    dsObj

--- Destructuring_Tricks/dsObject.js
+++ Destructuring_Tricks/dsObject.js
@@ -1,13 +1,23 @@
 const user = {firstname: 'John', lastname: 'sam'}
+
 const user2 = {Job : "software dev", exp:3}
 
 const user3 = {Job : "manager", exp:10}
 
+const user4 = {Name : "test",age:24}
+
 const {firstname,lastname} = user
 
-const {job,exp} = user2
+const {Job,exp} = user3
+
+// var Name = user4.Name
+// var age = user4.age
+const {Name,age} = user4;
+
+console.log(Name);
+
 
 console.log(firstname);
 
-console.log(job,exp);
+console.log(Job,exp);
 
