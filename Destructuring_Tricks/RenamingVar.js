57c99a301be8d45348e01c849c174dcfd27ca301
Rejoy01 <rejoywilson65@gmail.com>
2023-09-02 01:29:33 +0530

    renameVar

--- Destructuring_Tricks/RenamingVar.js
+++ Destructuring_Tricks/RenamingVar.js
@@ -10,6 +10,7 @@ const user2 = {firstname:"test",lastname : 'test2'}
 
 const {firstname:first, lastname:last} = user2
 const {firstname:name} = user2
+const {firstname:newName} = user2
 console.log(first,last);
 
 //jhon
