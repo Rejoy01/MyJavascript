6f0a0704a4d46bfcb412cac40b162d693b50570a
Rejoy01 <rejoywilson65@gmail.com>
2023-11-18 23:40:05 +0530

    oops

--- OOPS/abstraction.js
+++ OOPS/abstraction.js
@@ -20,3 +20,19 @@ let cat = new Animal('Cat', 'Meow');
 // Using abstraction
 console.log(dog.getName()); // Output: "Dog"
 console.log(cat.makeSound()); // Output: "Meow"
+
+function NewAnimal(name, sound) {
+
+    // Public methods using 'this'
+    this.getName = function () {
+        return name;
+    };
+
+    this.makeSound = function () {
+        return sound;
+    };
+}
+
+// Creating instances
+let dog1 = new NewAnimal('Dog', 'Woof');
+let cat2 = new NewAnimal('Cat', 'Meow');
