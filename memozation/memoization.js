eaedcbc4e91eaf31e7a72b5fd403987b959e913f
Rejoy01 <rejoywilson65@gmail.com>
2023-12-05 21:30:52 +0530

    memoization

---
+++ memozation/memoization.js
@@ -0,0 +1,18 @@
+
+
+function MultiplyByTwo(n) {
+    if (MultiplyByTwo.cache === undefined) {
+      MultiplyByTwo.cache = {};
+    }
+  
+    if (MultiplyByTwo.cache.hasOwnProperty(n)) {
+      console.log(MultiplyByTwo.cache[n]+"ccccc"); // If result for n is already calculated, return it from cache
+    } else {
+      const result = n * 2; // Expensive calculation
+      MultiplyByTwo.cache[n] = result; // Store the result in cache
+      console.log(result);
+    }
+  }
+MultiplyByTwo(3);
+MultiplyByTwo(5);
+MultiplyByTwo(3);
